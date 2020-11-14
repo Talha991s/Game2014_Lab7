6eec3fa092a760d21b7a3bcc0e595468a57d244f
Talha991s <55731371+Talha991s@users.noreply.github.com>
2020-11-13 18:10:27 -0800

    Added the ramp and fix the move of the enemy on ramp direction.

--- Assets/_Scripts/OpossumBehaviour.cs
+++ Assets/_Scripts/OpossumBehaviour.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum RampDirection
+{
+    NONE,
+    UP,
+    DOWN
+}
+
+
 public class OpossumBehaviour : MonoBehaviour
 {
     public float runforce;
@@ -11,12 +19,15 @@ public class OpossumBehaviour : MonoBehaviour
     public LayerMask collisionGroundLayer;
     public LayerMask CollisionWallLayer;
     public bool isGroundAhead;
+    public bool OnRampHit;
+    public RampDirection rampDir;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        rampDir = RampDirection.NONE;
     }
 
     // Update is called once per frame
@@ -27,17 +38,52 @@ public class OpossumBehaviour : MonoBehaviour
         _Move();
     }
 
+    //looking ahead
     private void _LookAhead()
     {
-        isGroundAhead = Physics2D.Linecast(transform.position, LookAheadPoint.position, collisionGroundLayer);
+        var GroundHit = Physics2D.Linecast(transform.position, LookAheadPoint.position, collisionGroundLayer);
+
+        if (GroundHit)
+        {
+            if(GroundHit.collider.CompareTag("Ramps"))
+            {
+                OnRampHit = true;
+            }
+
+            if(GroundHit.collider.CompareTag("Platform"))
+            {
+                OnRampHit = false;
+            }
+            isGroundAhead = true;
+        }
+        else
+        {
+            isGroundAhead = false;
+        }
         Debug.DrawLine(transform.position, LookAheadPoint.position, Color.green);
     }
 
+    //looking in front 
     private void _LookInFront()
     {
-        if (Physics2D.Linecast(transform.position, LookInFrontPoint.position, CollisionWallLayer))
+        var wallhit = Physics2D.Linecast(transform.position, LookInFrontPoint.position, CollisionWallLayer);
+
+        if (wallhit)
         {
-            _flipX();
+            if(!wallhit.collider.CompareTag("Ramps"))
+            {
+                if(!OnRampHit && transform.rotation.z == 0)
+                {
+                    _flipX();
+                }
+               
+                rampDir = RampDirection.DOWN;
+            }
+            else
+            {
+                rampDir = RampDirection.UP;
+            }
+          
         }
         
         Debug.DrawLine(transform.position, LookInFrontPoint.position, Color.black);
@@ -48,6 +94,24 @@ public class OpossumBehaviour : MonoBehaviour
         if(isGroundAhead)
         {
             rigidbody2D.AddForce(Vector2.left * runforce * Time.deltaTime *transform.localScale.x);
+
+            if(OnRampHit)
+            {
+                if(rampDir == RampDirection.UP)
+                {
+                    rigidbody2D.AddForce(Vector2.up * runforce * 0.5f * Time.deltaTime);
+                }
+                else
+                {
+                    rigidbody2D.AddForce(Vector2.down * runforce * 0.5f * Time.deltaTime);
+                }
+                StartCoroutine(Rotate());
+            }
+            else 
+            {
+                StartCoroutine(Normalize());
+            }
+
             rigidbody2D.velocity *= 0.90f;
         }
         else
@@ -56,6 +120,19 @@ public class OpossumBehaviour : MonoBehaviour
         }
 
     }
+
+    IEnumerator Rotate()
+    {
+        yield return new WaitForSeconds(0.05f);
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, -26.0f);
+    }
+
+    IEnumerator Normalize()
+    {
+        yield return new WaitForSeconds(0.05f);
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+    }
+
     private void _flipX()
     {
         transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
